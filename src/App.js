e5cea2e9238fdf2f52de3044d9875f3c379c9ea5
Francisco Dominguez Guerrero <48119418+pacod8@users.noreply.github.com>
2025-12-01 15:33:43 +0100

    fully working app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import Sponsors from "./Components/Sponsors";
 import Eventos from "./Components/Eventos";
 import Header from "./Components/Header";
 import Partidos from "./Components/Partidos";
+import PantallaSportmember from "./Components/PantallaSportmember";
 import "./App.css";
 
 function App() {
   return (
-  <>
+    <>
       <Header />
 
       <div className="layout">
         <div className="left-column">
           <Eventos />
-          <Partidos/>
+          <Partidos />
         </div>
 
         <div className="right-column">
-
+          <PantallaSportmember />
         </div>
       </div>
 
---
+++ src/Components/PantallaSportmember.jsx
@@ -0,0 +1,17 @@
+import "../App.css";
+
+function PantallaSportmember() {
+    return (
+      <div className="sportmember-frame">
+        <iframe
+          src="https://www.sportmember.es/info_screens/2330"
+          title="Sportmember InfoScreen"
+          frameBorder="0" 
+          allowFullScreen
+        />
+      </div>
+    );
+  }
+  
+  export default PantallaSportmember;
+  
